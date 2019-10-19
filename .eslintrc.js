9bbdab161240a323b4b71b7211d21d26085eda58
Gavin Chua <me@gavinchua.com>
2019-10-19 22:59:29 +0800

    eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,31 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
+    es6: true,
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended'
   ],
   rules: {
+    'arrow-spacing': ['error', { 'before': true, 'after': true }],
+    'comma-dangle': ['error', {
+      'imports': 'never',
+      'exports': 'never',
+      'functions': 'ignore'
+    }],
+    'curly': 'error',
+    'indent': ['error', 2],
+    'keyword-spacing': ['error', { 'before': true, 'after': true }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'object-curly-spacing': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'space-before-blocks': ['error', 'always'],
+    'space-before-function-paren': ['error', 'never']
   },
   parserOptions: {
     parser: 'babel-eslint'
