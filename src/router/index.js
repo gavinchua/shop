6e86d41202c8d55a801ad5a33f14c4cf7be5b780
Gavin Chua <me@gavinchua.com>
2019-10-20 16:19:15 +0800

    add new routes and router guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store'
 
 import Home from '@/views/Home'
 
@@ -12,20 +13,79 @@ const routes = [
     component: Home
   },
   {
+    path: '/about-us',
+    name: 'aboutus',
+    component: () => import(
+      /* webpackChunkName: "aboutus" */
+      '../views/AboutUs'
+    )
+  },
+  {
+    path: '/locations',
+    name: 'locations',
+    component: () => import(
+      /* webpackChunkName: "locations" */
+      '../views/Locations'
+    )
+  },
+  {
+    path: '/contact-us',
+    name: 'contactus',
+    component: () => import(
+      /* webpackChunkName: "contactus" */
+      '../views/ContactUs'
+    )
+  },
+  {
+    path: '/faqs',
+    name: 'faqs',
+    component: () => import(
+      /* webpackChunkName: "faqs" */
+      '../views/FAQs'
+    )
+  },
+  {
+    path: '/faqs',
+    name: 'faqs',
+    component: () => import(
+      /* webpackChunkName: "faqs" */
+      '../views/FAQs'
+    )
+  },
+  {
+    path: '/help-desk',
+    name: 'helpdesk',
+    component: () => import(
+      /* webpackChunkName: "helpdesk" */
+      '../views/HelpDesk'
+    )
+  },
+  {
+    path: '/customer-service',
+    name: 'customerservice',
+    component: () => import(
+      /* webpackChunkName: "customerservice" */
+      '../views/CustomerService'
+    )
+  },
+  {
+    path: '/subscribe',
+    name: 'subscribe',
+    component: () => import(
+      /* webpackChunkName: "subscribe" */
+      '../views/Subscribe'
+    )
+  },
+  {
     path: '/market',
     name: 'market',
     component: () => import(
       /* webpackChunkName: "market" */
       '../views/Market'
-    )
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import(
-      /* webpackChunkName: "about" */
-      '../views/About'
-    )
+    ),
+    meta: {
+      requiresLogin: true
+    }
   },
   {
     path: '/account',
@@ -33,7 +93,10 @@ const routes = [
     component: () => import(
       /* webpackChunkName: "account" */
       '../views/Account'
-    )
+    ),
+    meta: {
+      requiresLogin: true
+    }
   },
   {
     path: '*',
@@ -43,6 +106,20 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(
+    record => record.meta.requiresLogin
+  )) {
+    if (store.getters['appUser/getUserLogin']) {
+      next()
+      return
+    }
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
